d46f618a06fddeac7d1007ad2b89c825d8f01df8
Owen-Schade <152450132+Owen-Schade@users.noreply.github.com>
2026-02-19 12:24:51 -0600

    Enhance comments for shader program initialization
    Added comments to clarify shader program initialization.

--- webgl-demo.js
+++ webgl-demo.js
@@ -37,6 +37,9 @@ function main() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
+  // Initialize a shader program; this is where all the lighting
+  // for the vertices and so forth is established.
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 }
 
 //
